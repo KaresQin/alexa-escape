d62dea0b49a804634c848d54ff87689330b7f9ca
Becky <gysbecky@gmail.com>
2016-04-16 21:11:52 -0700

    start code

---
+++ MapStorage.js
@@ -0,0 +1,30 @@
+'use strict';
+var AWS = require("aws-sdk");
+AWS.config.update({accessKeyId: 'AKIAI622QKRBOFAI5CVQ', secretAccessKey: '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'});
+AWS.config.update({region: 'us-east'});
+
+function loadGame(session, callback) {
+// var storage = (){
+	var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+	
+	function PuzzelGame(session, data) {
+        if (data) {
+            this.data = data;
+        } else {
+            this.data = {
+                position : [0,0],
+            };
+        }
+        this._session = session;
+    }
+
+	//load user location
+	dynamodb.getItem({
+        TableName: 'CurrentLocationKeeper',
+                Key: {
+                    UserId: {
+                        S: session.user.userId
+                    }
+                }
+    }
+};
---
+++ PuzzleIntentHandlers.js
@@ -0,0 +1,44 @@
+'use strict';
+var textHelper = require('./textHelper'),
+    storage = require('./storage');
+
+var registerIntentHandlers = function (intentHandlers, skillContext) {
+
+	intentHandlers.MoveNorthIntent = function (intent, session, response) {
+        
+        storage.loadGame(session, function (currentGame) {
+        	if(!currentGame.data.position[0] && !currentGame.data.position[1]){
+        		response.ask('New game started. Who\'s your first player?',
+                    'Please tell me who\'s your first player?');
+        		return;
+        	}
+        });
+
+        var clonedArray = currentGame.data.position.concat();
+        clonedArray[0] -= 1;
+        checkNextStatus(clonedArray[0], clonedArray[1]);
+        // var array = string.split(',');
+        // var x = array[0];
+        // var y = array[1];
+        // x -= 1;
+        // currentLocation = x + "," + y;
+
+
+
+    };
+
+
+
+
+
+
+};
+
+function checkNextStatus(x, y){
+		//0 means wall
+		if(map[x][y] == 0){
+			return false;
+		} else{
+			return map[x][y];
+		}
+}
